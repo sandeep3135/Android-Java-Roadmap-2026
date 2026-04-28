1630e240e1685b7aeae48555f96c515dc782054e
sandeep3135 <sandeep7808822926@gmail.com>
2026-04-29 00:51:14 +0530

    Solved data types and variables Examples

--- src/Java_Example_Question/task5.java
+++ src/Java_Example_Question/task5.java
@@ -1,4 +1,22 @@
 package Java_Example_Question;
 
+/*Task 5: Batch Variable Initializer
+
+The Scenario: You are calculating the storage volume required for a shipping container in a warehouse.
+
+Requirement: On one single line, initialize three int variables: width = 10, height = 20, and depth = 30.
+
+Goal: 1. Calculate the volume: width * height * depth.
+2. Print: "Container Volume: [result] cubic meters".*/
+
 public class task5 {
+    public static void main(String[] args) {
+
+        int width =10, height = 20, depth = 30;
+
+        int volume = width * height * depth;
+
+        System.out.print("Container Volume: " + volume + " cubic meters");
+
+    }
 }
