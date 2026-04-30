9f83b92f603685e87cb90759e1e1c7d60cc445cd
sandeep3135 <sandeep7808822926@gmail.com>
2026-04-30 13:41:16 +0530

    Solved 2d array practicals ex.

--- src/Array/multidimensionalArray.java
+++ src/Array/multidimensionalArray.java
@@ -1,4 +1,26 @@
 package Array;
 
+import java.util.Scanner;
+
 public class multidimensionalArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int row = sc.nextInt();
+        int cols = sc.nextInt();
+
+        int numbers[][] = new int[row][cols];
+
+        for (int i = 0; i<row; i++ ) {
+            for (int j =0; j<cols; j++) {
+                numbers[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i<row; i++) {
+            for (int j = 0; j<cols; j++){
+                System.out.print(numbers[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
