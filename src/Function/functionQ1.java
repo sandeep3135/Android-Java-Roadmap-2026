07c7191cbc30f918ca37a76751ed66f22fe916d3
sandeep3135 <sandeep7808822926@gmail.com>
2026-05-01 12:29:43 +0530

    Solved sum of 2 no ex. questions using function

--- src/Function/functionQ1.java
+++ src/Function/functionQ1.java
@@ -1,4 +1,23 @@
 package Function;
 
+// Make a function to add 2 numbers and return the sum:
+
+import java.util.Scanner;
+
 public class functionQ1 {
+    public static int calculateSum(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int sum = calculateSum(a, b);
+        System.out.println("sum of given 2 no is: " + sum);
+    }
+
 }
