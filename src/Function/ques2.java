3a6fb04e125fa7a519603c46090e5f42c9ccb67e
sandeep3135 <sandeep7808822926@gmail.com>
2026-05-02 11:15:27 +0530

    create a function to give the output double of given input

--- src/Function/ques2.java
+++ src/Function/ques2.java
@@ -1,4 +1,22 @@
 package Function;
 
+//using the function return the output value is double of the given number:
+
+import java.util.Scanner;
+
 public class ques2 {
+
+    public static int doubleGame(int a) {
+        return a * 2;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+
+        int myDouble = doubleGame(a);
+        System.out.println(myDouble);
+
+    }
 }
