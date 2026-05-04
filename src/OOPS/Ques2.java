5fefaa321cb4a4b4f6ada854e705d86b942b433e
sandeep3135 <sandeep7808822926@gmail.com>
2026-05-04 10:46:31 +0530

    practice methods example ques.

--- src/OOPS/Ques2.java
+++ src/OOPS/Ques2.java
@@ -1,4 +1,19 @@
 package OOPS;
 
 public class Ques2 {
+
+    public void fullThrottle(){
+        System.out.println("The car is going as fast as it can!");
+    }
+
+    public void speed(int a){
+        System.out.println("max speed: "+a);
+    }
+
+    public static void main(String[] args) {
+
+        Ques2 myCar = new Ques2();
+        myCar.fullThrottle();
+        myCar.speed(150);
+    }
 }
