e83863a60be4ad38d2096c00a2759eaa92774e44
sandeep3135 <sandeep7808822926@gmail.com>
2026-05-04 10:46:04 +0530

    practice methods example ques.

---
+++ src/OOPS/Ques1.java
@@ -0,0 +1,25 @@
+package OOPS;
+
+public class Ques1 {
+    public static void main(String[] args) {
+
+        Dog d1 = new Dog();
+      //  d1.bark();
+        System.out.println(d1.name + " dog is barking and his age is " + " "+ d1.age + " " +d1.yes);
+    }
+}
+
+class Dog{
+
+    String name = "pk";
+    int age = 25;
+    boolean yes = true;
+
+    void bark(){
+        System.out.println(name + " dog is barking and his age is " + age);
+    }
+
+    void run(){
+        System.out.println("dog is running");
+    }
+}
