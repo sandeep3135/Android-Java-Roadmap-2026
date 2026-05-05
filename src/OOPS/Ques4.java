1e8e69e8f491aec7a840bd968c8b0cf65d0e7fa2
sandeep3135 <sandeep7808822926@gmail.com>
2026-05-05 22:06:47 +0530

    pass many parameters inside constructors

--- src/OOPS/Ques4.java
+++ src/OOPS/Ques4.java
@@ -1,4 +1,24 @@
 package OOPS;
 
-public class Ques4 {
+//pass many parameters as you want in constructor:
+
+import java.sql.SQLOutput;
+
+public class Ques4{
+
+   String name;
+    int age;
+
+    public Ques4(String name1, int age1) {
+        name = name1;
+         age = age1;
+    }
+
+
+
+    public static void main(String[] args) {
+
+        Ques4 s1 = new Ques4("Sandeep", 21);
+        System.out.println(s1.name +" " + s1.age);
+    }
 }
