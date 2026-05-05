ed6ee7aab1b65bf0dbe2b325b17e085291bc26b1
sandeep3135 <sandeep7808822926@gmail.com>
2026-05-05 21:31:20 +0530

    create constructor class for main class

--- src/OOPS/Ques3.java
+++ src/OOPS/Ques3.java
@@ -1,4 +1,19 @@
 package OOPS;
 
+// Create a class constructor for the Main class
+
 public class Ques3 {
+
+    int x;
+
+    public Ques3() {
+        x = 5;
+    }
+
+
+    public static void main(String[] args) {
+
+        Ques3 s1 = new Ques3();
+        System.out.println(s1.x);
+    }
 }
