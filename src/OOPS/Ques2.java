9d97c5ce6b96ad895f40bf6d85a0b7ad99251d2a
sandeep3135 <sandeep7808822926@gmail.com>
2026-05-05 21:30:33 +0530

    change method name

--- src/OOPS/Ques2.java
+++ src/OOPS/Ques2.java
@@ -1,8 +1,10 @@
 package OOPS;
 
+//Create a Car object named myCar. Call the fullThrottle() and speed() methods on the myCar object, and run the program:
+
 public class Ques2 {
 
-    public void fullThrottle(){
+    public void FullThrottle(){
         System.out.println("The car is going as fast as it can!");
     }
 
@@ -13,7 +15,7 @@ public class Ques2 {
     public static void main(String[] args) {
 
         Ques2 myCar = new Ques2();
-        myCar.fullThrottle();
+        myCar.FullThrottle();
         myCar.speed(150);
     }
 }
