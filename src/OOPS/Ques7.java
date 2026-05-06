9d04133f8d4dbebd652e419ec842dc62db7c01fa
sandeep3135 <sandeep7808822926@gmail.com>
2026-05-06 13:15:38 +0530

    use of public and private modifiers

--- src/OOPS/Ques7.java
+++ src/OOPS/Ques7.java
@@ -1,4 +1,19 @@
 package OOPS;
 
+//use of public and private modifiers
+
+
 public class Ques7 {
+    public static void main(String[] args) {
+        person man = new person();
+        System.out.println(man.name);
+        System.out.println(man.age);  //it gives an Error: age has private access in Person
+
+    }
+}
+
+
+class person{
+    public String name = "Ishan";
+    private int age = 21;
 }
