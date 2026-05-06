6efa7550fe2f7877fa9b46b33c1cefa6bc77ea2f
sandeep3135 <sandeep7808822926@gmail.com>
2026-05-06 13:11:30 +0530

    used this keywords to call class attributes

---
+++ src/OOPS/Ques5.java
@@ -0,0 +1,17 @@
+package OOPS;
+
+//use of this. keywords in class
+
+public class Ques5 {
+
+    int x;
+
+    public Ques5(int x){
+        this.x = x;
+    }
+
+    public static void main(String[] args) {
+        Ques5 s1 = new Ques5(21);
+        System.out.println("value of x: " + s1.x);
+    }
+}
