48766d0148c8abf3566b8025c7aa1b5071bca946
sandeep3135 <sandeep7808822926@gmail.com>
2026-05-07 10:44:22 +0530

    use of statickeywords or modifiers

--- src/OOPS/Ques9.java
+++ src/OOPS/Ques9.java
@@ -1,4 +1,17 @@
 package OOPS;
 
+// use of static modifiers call it without creating an object of the class.
+
 public class Ques9 {
+
+    static void staticMethod(){
+
+        System.out.println("Static methods can be called without creating objects");
+    }
+
+    public static void main(String[] args) {
+
+        staticMethod();  // Call the static method
+        Ques9.staticMethod();  // Or call it using the class name
+    }
 }
