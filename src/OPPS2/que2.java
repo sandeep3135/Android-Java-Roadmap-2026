1ea4a3c9143c3ec291466f144329f77a7180f991
sandeep3135 <sandeep7808822926@gmail.com>
2026-05-08 11:21:34 +0530

    solve Encapsulation example uses of get and set method

--- src/OPPS2/que2.java
+++ src/OPPS2/que2.java
@@ -2,7 +2,8 @@ package OPPS2;
 
 //Encapsulation uses of get and set method
 
-public class ques2 {
+public class que2 {
+
 
 }
 
@@ -10,12 +11,25 @@ class Person {
 
     private String name;
     int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
 }
 
-public String getName(){
-    return name;
+
+
+class Boy{
+
+    public static void main(String[] args) {
+        Person obj = new Person();
+        obj.setName("Ishan");
+        obj.age = 21;
+        System.out.println(obj.getName() + " " + obj.age);
+    }
 }
 
-public void setName(String newName) {
-    this.
-}
