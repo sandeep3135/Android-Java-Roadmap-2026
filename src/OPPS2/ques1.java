a3dfd33bb84fe32c4259b42d1aa547616ff61c36
sandeep3135 <sandeep7808822926@gmail.com>
2026-05-08 11:23:19 +0530

    solve Inheritance class example

--- src/OPPS2/ques1.java
+++ src/OPPS2/ques1.java
@@ -1,4 +1,28 @@
 package OPPS2;
 
+//the Car class (subclass) inherits the attributes and methods from the Vehicle class (superclass):
+
 public class ques1 {
+}
+
+
+class vehicles{
+    protected String brand = "Ford";
+
+    public void honk(){
+        System.out.println("Piii, piii!");
+    }
+}
+
+class Car extends vehicles{
+    private String modelName = "Mustang";
+
+    public static void main(String[] args) {
+
+        Car obj = new Car();
+
+        obj.honk();   // here, method call from vehicles to car on object
+        System.out.println(obj.modelName + " " + obj.brand);
+
+    }
 }
