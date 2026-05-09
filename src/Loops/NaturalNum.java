9ce9122cf7437461bce8428757688162a5403e86
sandeep3135 <sandeep7808822926@gmail.com>
2026-05-09 12:18:26 +0530

    print natural number upto given limit

--- src/Loops/NaturalNum.java
+++ src/Loops/NaturalNum.java
@@ -1,4 +1,21 @@
 package Loops;
 
+// print natural number for given limits
+
 public class NaturalNum {
+
+    public void printNaturalNum(int limit){
+
+        for (int i = 1; i <= limit; i++){
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        NaturalNum myNo = new NaturalNum();
+
+        System.out.println("Total natural no upto: 15 ");
+        myNo.printNaturalNum(15);
+    }
 }
