d36a39b86f63ee9e2e0d76e77f529ef69efcd6d5
sandeep3135 <sandeep7808822926@gmail.com>
2026-05-09 12:04:12 +0530

    Create uber fare price calculator

--- src/Java_Example_Question/UberFare.java
+++ src/Java_Example_Question/UberFare.java
@@ -1,4 +1,33 @@
 package Java_Example_Question;
 
 public class UberFare {
+
+    public void calculateFare(double distance, int hourOfDay){
+
+        double totalFare = 5.00;  //if you book cap fare is 5
+        totalFare +=(2.00 * distance);  // price for distance per/km
+
+        if (hourOfDay >= 22 || hourOfDay <= 5){
+            totalFare += 10.00;  //pay extra for night fee
+        }if(totalFare < 10.00) {
+            totalFare = 10.00;   // if your fair is less than 10 then fair is minimum 10
+        }
+        System.out.println("Distance: "+ distance +" Km," + " Time: " + hourOfDay + ":00");
+        System.out.println("Final fare price: $" + totalFare);
+    }
+
+
+    public static void main(String[] args) {
+        UberFare myTf = new UberFare();
+
+
+        // Day time (10 AM), 5km. (Expected: 5 + 10 = $15)
+        myTf.calculateFare(5, 10);
+
+        // Night time (Midnight), 2km. (Expected: 5 + 4 + 10 = $19)
+        myTf.calculateFare(2, 0);
+
+        // Short trip (1km), Day time. (Expected: 5 + 2 = 7, but Minimum is 10)
+        myTf.calculateFare(1, 14);
+    }
 }
