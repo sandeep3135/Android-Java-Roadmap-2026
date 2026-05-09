e274bea0777c8bcbe0d89e0c225991609d0934c2
sandeep3135 <sandeep7808822926@gmail.com>
2026-05-09 12:16:44 +0530

    sum of a given natural no

--- src/Loops/NumberSum.java
+++ src/Loops/NumberSum.java
@@ -1,4 +1,22 @@
 package Loops;
 
+// sum of a given limit of no
+
 public class NumberSum {
+
+    public void sumUpTo(int limit){
+
+        int sum = 0;
+
+        for (int i = 0; i <= limit; i++)
+            sum += i;
+        System.out.println("Sum of no: "+ sum);
+    }
+
+    public static void main(String[] args) {
+
+        NumberSum mySum = new NumberSum();
+
+        mySum.sumUpTo(5);
+    }
 }
