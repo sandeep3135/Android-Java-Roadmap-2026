9ce3ff00c03a3198efb0dacae11bc1ced6a51b22
sandeep3135 <sandeep7808822926@gmail.com>
2026-05-09 12:14:26 +0530

    Check the given no is prime or not

--- src/Loops/PrimeCheck.java
+++ src/Loops/PrimeCheck.java
@@ -1,4 +1,32 @@
 package Loops;
 
+// Check the given no is prime or not
+
 public class PrimeCheck {
+
+    public void isPrime(int number){
+
+        boolean isPrime = true;
+
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            System.out.println(number + " is a Prime number.");
+        } else {
+            System.out.println(number + " is NOT a Prime number.");
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+
+        PrimeCheck myP = new PrimeCheck();
+
+        myP.isPrime(47);
+    }
 }
