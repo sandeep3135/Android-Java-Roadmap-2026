a9608cddefa6cb6182b5e4d241afd88cd1512853
sandeep3135 <sandeep7808822926@gmail.com>
2026-05-09 12:12:09 +0530

    print a number in reverse order from a given starting point of a number

--- src/Loops/Countdown.java
+++ src/Loops/Countdown.java
@@ -1,4 +1,28 @@
 package Loops;
 
+// print a number in reverse order from a given starting point of a number
+
+import java.util.concurrent.Callable;
+
 public class Countdown {
+
+    public void startCount(int start){
+
+        for (int i = start; i >= 0; i--){
+            if (i == 0) {
+                System.out.println("Blast off! 🚀");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        Countdown myC = new Countdown();
+
+        System.out.println("Reverse number count from:15 ");
+        myC.startCount(15);
+    }
 }
