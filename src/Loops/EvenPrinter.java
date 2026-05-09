44804a6d7cad60e04dcd4bad67962ecd2dfb724a
sandeep3135 <sandeep7808822926@gmail.com>
2026-05-09 12:13:23 +0530

    filter even numbers using loop

--- src/Loops/EvenPrinter.java
+++ src/Loops/EvenPrinter.java
@@ -1,4 +1,22 @@
 package Loops;
 
+//filter even numbers using loop for given limited number
+
 public class EvenPrinter {
+
+    public void printEvens(int number){
+
+        for (int i = 1; i <= number; i++){
+            if (i % 2 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        EvenPrinter e1 = new EvenPrinter();
+
+        System.out.println("Printing evens up to 10:");
+        e1.printEvens(10);
+    }
 }
