150c526e8fd9e499378ce1c4e8da8b8946149acc
sandeep3135 <sandeep7808822926@gmail.com>
2026-05-09 12:19:28 +0530

    Create multiplication table using for-loop

--- src/Loops/MultiplicationTable.java
+++ src/Loops/MultiplicationTable.java
@@ -1,4 +1,24 @@
 package Loops;
 
+//Create multiplication table using for-loop
+
 public class MultiplicationTable {
+
+
+    public void printTable(int number){
+
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " x " + i + " = " + (number*i));
+        }
+    }
+
+    public static void main(String[] args) {
+
+        MultiplicationTable myTable = new MultiplicationTable();
+
+        System.out.println("Table of 2:");
+        myTable.printTable(2);
+    }
+
 }
