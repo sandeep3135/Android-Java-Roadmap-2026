eef351653b98aee84166e8745c1b9524c3ed9871
sandeep3135 <sandeep7808822926@gmail.com>
2026-05-10 21:57:23 +0530

    implement fully encapsulated BankManager with object array processing

--- src/Java_Example_Question/BankManager.java
+++ src/Java_Example_Question/BankManager.java
@@ -1,4 +1,53 @@
 package Java_Example_Question;
 
+//implement fully encapsulated BankManager with object array processing
+
 public class BankManager {
+    private String accountHolder;
+    private double balance;
+
+    public BankManager(String accountHolder, double balance){
+        this.accountHolder = accountHolder;
+        // Logic check: Balance shouldn't start negative
+        this.balance = (balance > 0) ? balance : 0;
+    }
+
+    public void deposit(double amount){
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposit for " + accountHolder + ": +$" + amount);
+        } else {
+            System.out.println("Invalid deposit for " + accountHolder);
+        }
+    }
+
+    public double getBalance(){
+        return balance;
+    }
+
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+
+    public static void main(String[] args) {
+        // 1. Create the data list
+        BankManager[] allAccount = new BankManager[4];
+        allAccount[0] = new BankManager("Ishan", 500);
+        allAccount[1] = new BankManager("Raj", 800);
+        allAccount[2] = new BankManager("Amit", 300);
+        allAccount[3] = new BankManager("Sandeep", 1000);
+
+        // 2. Perform actions
+        allAccount[0].deposit(200);
+        allAccount[3].deposit(50);
+
+        System.out.println("\n--- FINAL BANK REPORT ---");
+
+        // 3. Loop through to see EVERYONE'S unique data
+        for (int i = 0; i < allAccount.length; i++) {
+            System.out.println("Customer: " + allAccount[i].getAccountHolder() +
+                    " | Balance: $" + allAccount[i].getBalance());
+        }
+    }
 }
+
